5274790c409872c0bafe50f396a116ea387edda0
Jason Huang <jason17huang@gmail.com>
2019-06-22 22:20:21 -0400

    Deleted cards

--- hackerhub/views.py
+++ hackerhub/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from hackerhub.models import Hackathon
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def index(request):
+
     hackathons = Hackathon.objects.order_by('startDate').reverse()
     template_name = 'index.html'
     context = {
